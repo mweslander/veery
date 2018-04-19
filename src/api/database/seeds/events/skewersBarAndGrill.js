24d7d9a7ec6c9a8e578e33895ccd14a235944e52
Jordan Hammond <jahammo2@gmail.com>
2018-04-19 18:49:18 +0200

    updated last seeds

--- src/api/database/seeds/events/skewersBarAndGrill.js
+++ src/api/database/seeds/events/skewersBarAndGrill.js
@@ -7,11 +7,12 @@ function events() {
   return Venue
     .findOne({ name: 'Skewers Bar and Grill' })
     .then((venue) => {
+      // This can now be scraped
       return [
         {
           frequency: 'weekly',
           startDate: moment().isoWeekday(3).format('MM-DD-YYYY'),
-          startTime: '9:00pm',
+          startTime: '10:00pm',
           title: 'Open Mic Night',
           type: 'open',
           venue: venue._id
