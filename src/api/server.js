3df42d7e775358d329a1b3c68f21373f6e9af973
Jordan Hammond <jahammo2@gmail.com>
2018-08-29 19:51:23 -0600

    updated port listening

--- src/api/server.js
+++ src/api/server.js
@@ -2,6 +2,6 @@
 
 const app = require('./config/app.js');
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('💸 App listening on port 3000'); // eslint-disable-line
 });
