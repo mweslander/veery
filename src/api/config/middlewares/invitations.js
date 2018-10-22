ec6504127a3faf76b612a7fc07e22be773647e71
Jordan Hammond <jahammo2@gmail.com>
2018-10-21 22:03:27 -0600

    reducing cognitive complexity according to code climate

--- src/api/config/middlewares/invitations.js
+++ src/api/config/middlewares/invitations.js
@@ -3,21 +3,28 @@
 const _ = require('lodash');
 const User = require('../../app/models/user');
 
-function ensureVenueAccessibility(res, done, { venues }) {
-  return function(user) {
-    const stringedVenues = user.venues.map((v) => v.toString());
-    const inaccessibleVenues = venues.filter((venue) => {
-      return !_.includes(stringedVenues, venue);
-    });
+function ensureVenueAccessibility(res, done, { user, venues }) {
+  // To ensure we have the most accurate attributes on the user
+  return User
+    .findById(user && user._id)
+    .then((_user) => {
+      const stringedVenues = _user.venues.map((v) => v.toString());
+      const inaccessibleVenues = venues.filter((venue) => {
+        return !_.includes(stringedVenues, venue);
+      });
 
-    const userCantAccessVenue = user.role !== 'admin' && inaccessibleVenues.length > 0;
+      const userCantAccessVenue = _user.role !== 'admin' && inaccessibleVenues.length > 0;
 
-    if (userCantAccessVenue) {
-      return done(res.status(401).send(), false);
-    }
+      if (userCantAccessVenue) {
+        return done(res.status(401).send(), false);
+      }
 
-    return done(null);
-  };
+      return done(null);
+    })
+    // if a user is being searched for, then that's bc there were
+    // venues attached. So if it isn't found, that means that the
+    // user, whom is non existent, can't modify those venue(s)
+    .catch(() => done(res.status(401).send(), false));
 }
 
 function ensureAccessibility(req, res, done) {
@@ -35,14 +42,7 @@ function ensureAccessibility(req, res, done) {
     return done(null);
   }
 
-  // To ensure we have the most accurate attributes on the user
-  return User
-    .findById(user && user._id)
-    .then(ensureVenueAccessibility(res, done, body))
-    // if a user is being searched for, then that's bc there were
-    // venues attached. So if it isn't found, that means that the
-    // user, whom is non existent, can't modify those venue(s)
-    .catch(() => done(res.status(401).send(), false));
+  return ensureVenueAccessibility(res, done, { user, venues: body.venues });
 }
 
 module.exports = {
